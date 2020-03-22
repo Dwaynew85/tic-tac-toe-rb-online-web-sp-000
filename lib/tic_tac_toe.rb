282c37d943c2347df6f2562fc7f9e13cc8c057e4
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-03-22 06:06:33 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -53,3 +53,15 @@ def current_player(board)
   end
   return player
 end
+
+def turn (board)
+  puts "Please enter 1-9:"
+  num = gets.chomp
+  index = input_to_index(num)
+  if valid_move?(board, index) == true
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
