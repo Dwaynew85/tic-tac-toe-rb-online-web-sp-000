5e3860c95691f9dd6e4de21a23a93f2dce89a945
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-03-22 06:08:52 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -58,7 +58,7 @@ def turn (board)
   num = gets.chomp
   index = input_to_index(num)
   if valid_move?(board, index) == true
-    move(board, index, current_player)
+    move(board, index)
     display_board(board)
   else
     turn(board)
