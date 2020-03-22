0e47bcad8270c2beea64795b27db6d4cbe32e73b
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-03-22 05:54:48 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -32,11 +32,5 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  def position_taken?(array, ind)
-    if array[ind] == " " || array[ind] == "" || array[ind] == nil
-      return false
-    else
-      return true
-    end
-  end
+  index.between?(0,8) && !position_taken?(board, index)
 end
